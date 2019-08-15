aa31dcf47238519b8e22a762d6142019b057e12d
JOHN NELSON <JohnNelson4850@gmail.com>
2019-08-15 14:36:44 -0700

    starting to layout components

---
+++ src/components/app/app.js
@@ -0,0 +1,27 @@
+import Component from '..Component.js';
+
+
+
+class App extends Component {
+
+    onRender() {
+    }
+
+
+    renderHTML() {
+        return /*html*/`
+            <div>
+               <!-- header goes here --> 
+                <main id="main">
+                    
+                </main>
+
+                <footer> <h3>&copy John</h3> </footer>
+            </div> 
+        `;
+    }
+
+
+}
+
+export default App;
---
+++ src/components/header/header.js
@@ -0,0 +1,16 @@
+import Component from '../Component.js';
+
+class NewComponent extends Component {
+    onRender() {
+        
+    }
+
+
+    renderHTML() {
+        return /*html*/`
+            
+        `;
+    }
+}
+
+export default NewComponent;
