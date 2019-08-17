c7e367506cada7968ae1ce45d90ff2b24b53303f
JOHN NELSON <JohnNelson4850@gmail.com>
2019-08-16 17:14:01 -0700

    api fetch

--- src/components/poke-card/pokemon.js
+++ src/components/poke-card/pokemon.js
@@ -3,7 +3,11 @@ import Component from '../Component.js';
 class Pokemon extends Component {
     renderHTML() {
         return /*html*/`
-            
+        <h2 id="pokemon">pichu</h2>
+        <img src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/172.png">
+        <div id="type">type: electric</div>
+        <div id="attack">attack: 35</div>
+        <div id="defense">defense: 35</div>
         `;
     }
 }
